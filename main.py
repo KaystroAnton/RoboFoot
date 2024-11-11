15c2573978ba506af2b13cf698ee1af8f36080fd
KaystroAnton <anton.kaystro@mail.ru>
2024-11-11 13:22:13 +0300

    add file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pybullet as pb
 import pybullet_data
 import time
+import cv2
 
 # parameters of simulation
 L = 0.2    # length of the corpus
@@ -10,17 +11,17 @@ dx = 0.1    # distance from wheel axle to center
 alpha = 0   # the angle of the robot's course in radians (relative to the X axis)
 dt = 1/240  # pybullet simulation step
 g = -9.8    # Gravity force
-IMG_SIDE = 2
+IMG_SIDE = 8
 targetPosition = [[10,-15],0,0]    # [[positon x,y], oriantation angel, velocity] the aim of the robot
 
 
 physicsClient = pb.connect(pb.GUI)  # pb.GUI for graphical version
 
 pb.resetDebugVisualizerCamera(
-    cameraDistance=3,
+    cameraDistance=1,
     cameraYaw=-90,
     cameraPitch=-89.999,    # No image if set on 90
-    cameraTargetPosition=[0.0, 0.0, 3.0]
+    cameraTargetPosition=[0.0, 0.0, -0.78]
 )   # Set сamera directly above the field
 
 pb.setGravity(0,0, g)
@@ -32,6 +33,12 @@ field = pb.loadURDF("field.urdf",[0,0,-0.1])
 c = pb.loadURDF('aruco.urdf', (0.0, 0.0, 0.0), useFixedBase=True)
 x = pb.loadTexture('aruco_cube.png')
 pb.changeVisualShape(c, -1, textureUniqueId=x)
+
+#init aruco detector
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+parameters = cv2.aruco.DetectorParameters()
+parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
 
 # get rid of all the default damping forces
@@ -57,8 +64,8 @@ while True:
         width=IMG_SIDE,
         height=IMG_SIDE,
         viewMatrix=pb.getDebugVisualizerCamera()[2],
-        projectionMatrix=pb.getDebugVisualizerCamera()[3],
-        renderer=pb.ER_BULLET_HARDWARE_OPENGL
+        projectionMatrix=pb.computeProjectionMatrixFOV(fov=45, aspect=1, nearVal=0.1, farVal=100.0),
+        renderer=pb.ER_TINY_RENDERER
     )
 
     #print(alpha)
