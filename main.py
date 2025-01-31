2fc0e449e5dc98b7d5bec7d9ee643b2a18495e56
KaystroAnton <anton.kaystro@mail.ru>
2025-01-31 18:44:33 +0300

    add file

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import cv2
 import math
 import numpy as  np
+from pybullet import getLinkState, getNumJoints
+
 import help
 pb.isNumpyEnabled()
 
@@ -15,7 +17,7 @@ alpha = 0   # the angle of the robot's course in radians (relative to the X axis
 dt = 1/240  # pybullet simulation step
 g = -9.8    # Gravity force
 cameraHeight = 3.0 # Z coordinate of camera
-IMG_SIDE = 400
+IMG_SIDE = 200
 halfFieldSize = 4.0/2
 accuracy = 0.0005
 kd = 20 # koeff for control
@@ -41,12 +43,13 @@ camera_proj_matrix = pb.computeProjectionMatrixFOV(
 
 pb.setGravity(0,0, g)
 
+# load robots and field
 field = pb.loadURDF("field.urdf",[0,0,0])
-#robot1 = pb.loadURDF("robot.urdf",[0,0,0])
+robot1 = pb.loadURDF("robot.urdf",[0,0,0.25])
 
 # add aruco cube and aruco texture
 c1 = pb.loadURDF('aruco.urdf', (halfFieldSize-0.15, halfFieldSize-0.15, 0.0), useFixedBase=True)
-c2 = pb.loadURDF('aruco.urdf', (0, 0, 0.0), useFixedBase=True)
+c2 = pb.loadURDF('aruco.urdf', (-1, 0, 0.0), useFixedBase=True)
 x = pb.loadTexture('aruco_cube.png')
 pb.changeVisualShape(c1, -1, textureUniqueId=x)
 pb.changeVisualShape(c2, -1, textureUniqueId=x)
@@ -59,12 +62,13 @@ detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
 
 # get rid of all the default damping forces
-#pb.changeDynamics(robot1, 1, linearDamping=0, angularDamping=0)
-#pb.changeDynamics(robot1, 2, linearDamping=0, angularDamping=0)
+pb.changeDynamics(robot1, 1, linearDamping=0, angularDamping=0)
+pb.changeDynamics(robot1, 2, linearDamping=0, angularDamping=0)
+pb.changeDynamics(robot1, 3, linearDamping=0, angularDamping=0)
 
 #   settings for robot
-#pb.setJointMotorControl2(bodyIndex=robot1, jointIndex=1, targetVelocity= 10, controlMode=pb.VELOCITY_CONTROL,force=100)
-#pb.setJointMotorControl2(bodyIndex=robot1, jointIndex=0, targetVelocity= 10, controlMode=pb.VELOCITY_CONTROL,force=100)
+pb.setJointMotorControl2(bodyIndex=robot1, jointIndex=1, targetVelocity= 10, controlMode=pb.VELOCITY_CONTROL,force=100)
+pb.setJointMotorControl2(bodyIndex=robot1, jointIndex=0, targetVelocity= 10, controlMode=pb.VELOCITY_CONTROL,force=100)
 #pb.getAxisAngleFromQuaternion(pb.getBasePositionAndOrientation(robot1)[1])
 #pb.resetBasePositionAndOrientation(robot1, posObj = [0,0,0], ornObj = pb.getQuaternionFromAxisAngle([0.0, 0.0, 1.0], 0))
 
@@ -75,6 +79,7 @@ while True:
     y=pb.getBasePositionAndOrientation(c2)[0][1]
     for j in range(100):
         while pow(x - xd,2) + pow(y - yd,2) > accuracy:
+            print(pb.getEulerFromQuaternion(getLinkState(bodyUniqueId=robot1, linkIndex=0)[1]))
             frame = pb.getCameraImage(
                 width=IMG_SIDE,
                 height=IMG_SIDE,
@@ -85,34 +90,25 @@ while True:
                 renderer=pb.ER_TINY_RENDERER
             ) # get frame from camera
             new_frame = help.get_frame(frame)
-            #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             (corners, ids, rejected) = detector.detectMarkers(new_frame)
-            print(corners, ids, rejected)
+            #print(corners, ids, rejected)
             omega = kd*(math.atan2((yd - y) , (xd - x)) - alpha)
             movement = [math.cos(alpha)*v*dt,v*dt*math.sin(alpha),0]
             newPosition = [pb.getBasePositionAndOrientation(c2)[0][i]+movement[i] for i in range(3)]
-            #print(pb.getEulerFromQuaternion(pb.getBasePositionAndOrientation(c2)[1]), "dfgdfg")
             pb.resetBasePositionAndOrientation(c2, newPosition, pb.getQuaternionFromEuler([0.0, 0.0, alpha+omega*dt]))
             alpha = round(pb.getEulerFromQuaternion(pb.getBasePositionAndOrientation(c2)[1])[2], 3)
             x = pb.getBasePositionAndOrientation(c2)[0][0]
             y = pb.getBasePositionAndOrientation(c2)[0][1]
-
-            img = cv2.UMat(new_frame)
-            cv2.aruco.drawDetectedMarkers(img, corners)
-            cv2.imshow('init', img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
             pb.stepSimulation()
             time.sleep(dt)
-        print("for", targetPosition, "is fine", j)
+        #print("for", targetPosition, "is fine", j)
         targetPosition = [[random.uniform(-2.0, 2.0),random.uniform(-2.0, 2.0)], 0.0]
-        print(targetPosition)
+        #print(targetPosition)
         xd = targetPosition[0][0]
         yd = targetPosition[0][1]
 
     pb.stepSimulation()
-    print(pb.getBasePositionAndOrientation(c2))
+    #print(pb.getBasePositionAndOrientation(c2))
     #print(pb.getEulerFromQuaternion(pb.getBasePositionAndOrientation(3)[1]), "dfgdfg")
     #print(pb.getAxisAngleFromQuaternion(pb.getBasePositionAndOrientation(3)[1]), "dfgdfg11111")
     # pb.ER_BULLET_HARDWARE_OPENGL pb.ER_TINY_RENDERER
